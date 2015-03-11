7c2285359b1710a35b93d0bcd1ae2f4be81f1bce
CJ Davis <cj@cjdavis.me>
2015-03-10 20:12:42 -0400

    Remove action bar and add location quest

--- app/src/main/java/com/cjdavis/iceholetracker/HomeScreen.java
+++ app/src/main/java/com/cjdavis/iceholetracker/HomeScreen.java
@@ -1,50 +1,131 @@
 package com.cjdavis.iceholetracker;
 
-import android.content.Context;
-import android.location.LocationManager;
-import android.support.v7.app.ActionBarActivity;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-public class HomeScreen extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
 
-    // ®LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+public class HomeScreen extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    public static final String TAG = HomeScreen.class.getSimpleName();
+
+    /*
+    * Define a request code to send to Google Play services
+    * This code is returned in Activity.onActivityResult
+    */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        return true;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_quit) {
-            finish();
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
     }
 
     public void GetGPSCoordinates(View v) {
-        Toast msg = Toast.makeText(getApplicationContext(), "Store GPS coordinates dang nabbit!", Toast.LENGTH_LONG);
-        msg.show();
+        try {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            }
+
+            String msgText = String.format("Lat: %1$s, Long: %2$s, Alt: %3$s, Acc: %4$s, Time: %5$s",
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    location.getAltitude(),
+                    location.getAccuracy(),
+                    location.getTime());
+            Toast msg = Toast.makeText(getApplicationContext(), msgText, Toast.LENGTH_LONG);
+            msg.show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), "Could not retrieve location information."
+                    + "\nPlease make sure location service is on and try again", Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
     public void SendGPSCoordinates(View v) {
